d59ae561505aa123de0cedee44f31886ca7d6ef8
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2020-03-17 05:20:10 -0700

    Enviar texto inserido pelo utilizador para outra tela.

--- app/src/main/java/com/example/mensagemalterado/MainActivity.java
+++ app/src/main/java/com/example/mensagemalterado/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.mensagemalterado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.mensagemalterado.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
